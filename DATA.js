9b20dda4cf6553ab17460ac39285b33f66d253b9
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-09-23 12:32:21 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "ec4059cb-9aa2-42e6-90b2-d263d8e299b1",
+    "home_feed": [
+        {
+            "id": "2cb21922-bc54-4fbe-ba46-ad040f2e4e83",
+            "artist_name": "tough",
+            "home_carousel": [
+                {
+                    "id": "859d7faf-07b1-43cc-b7ed-7d4c3efafaf9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png"
+                },
+                {
+                    "id": "807725ad-fcc2-4350-8fcf-77b0ea52c482",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/318.png"
+                },
+                {
+                    "id": "e2c859b0-8ed9-4150-8ce0-f7ab1af17b7d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/65.png"
+                },
+                {
+                    "id": "5ebcb799-6e80-4e60-8a02-5c0a49134c5a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png"
+                },
+                {
+                    "id": "3cf76ad8-91f2-4d0e-b655-4620e8d87aba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/129.png"
+                }
+            ],
+            "art_caption": "color",
+            "art_description": "Partner think top investment my."
+        },
+        {
+            "id": "6745ae50-99cb-414e-82c1-147171779bb1",
+            "artist_name": "establish",
+            "home_carousel": [
+                {
+                    "id": "5f8dd554-9d43-470c-88cb-361e70619905",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "9577af00-b5db-4cf4-b03e-66d7f3bbffd0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png"
+                },
+                {
+                    "id": "907c3346-4ff8-481c-ab65-32700d63377f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                },
+                {
+                    "id": "295a29d9-4ecd-49f6-8923-32b73d062d12",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/342.png"
+                },
+                {
+                    "id": "daac3de3-8d95-4369-9f53-682aa08c8d20",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                }
+            ],
+            "art_caption": "own",
+            "art_description": "Often first local."
+        },
+        {
+            "id": "3367285e-9c74-48f3-bc71-cdff0e2977b5",
+            "artist_name": "debate",
+            "home_carousel": [
+                {
+                    "id": "b58ca14a-ee3c-49aa-b28c-8e347ed63b68",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png"
+                },
+                {
+                    "id": "4d670514-4ecc-491a-8585-157464787139",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png"
+                },
+                {
+                    "id": "82b2ad00-2e46-4d9e-a9af-ee54b4453744",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/192.png"
+                },
+                {
+                    "id": "acabade7-5830-4dd5-b76f-0cc46205ac3a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/261.png"
+                },
+                {
+                    "id": "abaf08ac-ce5a-44c1-92ff-3c9160718614",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png"
+                }
+            ],
+            "art_caption": "evidence",
+            "art_description": "Interesting action street PM hand. Green consider range seat find."
+        },
+        {
+            "id": "e0783611-a4fa-4a36-bf9a-13cc1473dd18",
+            "artist_name": "pretty",
+            "home_carousel": [
+                {
+                    "id": "75e4007e-0d91-4d50-a980-6117152319c1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png"
+                },
+                {
+                    "id": "e31777b0-47dc-448d-83a8-4742d057dd40",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png"
+                },
+                {
+                    "id": "bec25d07-418c-4af1-bfe8-0289fc6a4014",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                },
+                {
+                    "id": "0f2b0ac4-587c-494d-9b94-b20979b417f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/248.png"
+                },
+                {
+                    "id": "dba6418e-5a5f-44b0-83d3-066945405997",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                }
+            ],
+            "art_caption": "show",
+            "art_description": "Him work class alone lawyer. Join court hear season."
+        },
+        {
+            "id": "f4d5549f-86c3-4951-a13f-26c7db2de758",
+            "artist_name": "shake",
+            "home_carousel": [
+                {
+                    "id": "b0a679d5-d11e-4068-adfa-1d764d5717ce",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                },
+                {
+                    "id": "d1b9533e-c314-418f-916a-be13f69530b7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "764e90d4-a337-4e10-a825-bc626fd8a949",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png"
+                },
+                {
+                    "id": "edbe79df-f0ac-40bc-809f-da85ab379a70",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/17.png"
+                },
+                {
+                    "id": "34e446ac-303a-4a4a-820b-4eb63243eb80",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                }
+            ],
+            "art_caption": "issue",
+            "art_description": "Manage successful much generation resource decision. Inside customer that house imagine hand particular."
+        }
+    ]
+}
